3c19b8763d14597b575aaca877d5b9744c4fa063
Renzo Osorio <renzo.osorio1909@gmail.com>
2022-12-19 19:13:28 -0500

    añadiendo componente NewExpense

--- react-complete-guide/src/App.jsx
+++ react-complete-guide/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 import "./App.css";
 
@@ -26,19 +27,19 @@ function App() {
     },
   ];
 
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h2", {}, "Let's get started!"),
-    React.createElement(Expenses, { items: expenses })
-  );
-
-  // return (
-  //   <div className="App">
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses} />
-  //   </div>
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
   // );
+
+  return (
+    <div className="App">
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
 }
 
 export default App;
--- react-complete-guide/src/components/Expenses/ExpenseItem.jsx
+++ react-complete-guide/src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
 
-function ExpenseItem({ title, amount, date }) {
+function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} title={title} amount={amount} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
