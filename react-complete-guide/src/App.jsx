4b08934bc44992522741c9ccfd35100b4b97fca4
Renzo Osorio <renzo.osorio1909@gmail.com>
2022-12-21 17:44:22 -0500

    estados en react y eventos

--- react-complete-guide/src/App.jsx
+++ react-complete-guide/src/App.jsx
@@ -27,6 +27,8 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {};
+
   // return React.createElement(
   //   "div",
   //   {},
@@ -36,7 +38,7 @@ function App() {
 
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
